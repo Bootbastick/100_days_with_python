4d1c0a526a4a00ddeb05cce25c070b0e25a15f4d
Bootbastick <boikoff.nikita@gmail.com>
2022-01-24 19:15:51 +0100

    Reproducing the Bug(after that fixing it)

--- Day13/debugging.py
+++ Day13/debugging.py
@@ -1,6 +1,12 @@
-#Describe Problem
-def my_function():
-    for i in range(1, 20):
-        if i == 19:
-            print("You got it")
-my_function()
+# #Describe Problem
+# def my_function():
+#     for i in range(1, 20):
+#         if i == 19:
+#             print("You got it")
+# my_function()
+
+# Reproduce the Bug
+from random import randint
+dice_imgs = ["1", "2", "3", "4", "5", "6"]
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
