f4eb9a2ffcf24c107e36d711bc76402999b6a74f
Bootbastick <boikoff.nikita@gmail.com>
2022-01-31 10:13:43 +0100

    Making part 1, 2 and 3 of the project

---
+++ Day17/main.py
@@ -0,0 +1,13 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+question_bank = []
+
+
+for question in range(len(question_data)):
+    new_q = Question(question_data[question]["question"], question_data[question]["correct_answer"])
+    question_bank.append(new_q)
+
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
