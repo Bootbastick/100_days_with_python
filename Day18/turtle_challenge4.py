466aaaa357dada505de6ac01e3fed47c32eae4c8
Bootbastick <boikoff.nikita@gmail.com>
2022-02-02 18:24:49 +0100

    Changing program to make random rgb colors

--- Day18/turtle_challenge4.py
+++ Day18/turtle_challenge4.py
@@ -2,14 +2,23 @@ import random
 from time import sleep
 import turtle
 t = turtle.Turtle()
-color_list = ["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple", "navy", "blue", "skyblue", "cyan", "turquoise", "lightgreen", "green", "darkgreen", "chocolate", "brown", "black", "gray1", "white"]
+turtle.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+
 directions = [0, 90, 180, 270]
 t.pensize(15)
 t.speed("fastest")
 
 
 for i in range(random.randint(200, 10000)):
-    t.color(random.choice(color_list))
+    t.color(random_color())
     t.forward(30)
     t.setheading(random.choice(directions))
 
