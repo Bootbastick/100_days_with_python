6173f2af2c1258d8e7de590a86b70fc4f2bc10c6
Bootbastick <boikoff.nikita@gmail.com>
2022-04-15 09:14:50 +0200

    Improving Nato phonetic alphabet and password manager

--- Day29/main.py
+++ Day29/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -37,24 +38,53 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if website == "" or email == "" or password == "":
         messagebox.showinfo(title="Oops", message="Please do not leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(
-            title=website,
-            message=f"These are the details you have entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?"
-        )
+        try:
+            with open("data.json", mode="r") as file:
+                data = json.load(file)
 
-        if is_ok:
-            with open("data.txt", mode="a") as file:
-                file.write(input_data + "\n")
+        except FileNotFoundError:
+            with open("data.json", mode="w") as file:
+                json.dump(new_data, file, indent=4)
+
+        else:
+            with open("data.json", mode="w") as file:
+                data.update(new_data)
+                json.dump(data, file, indent=4)
+
+        finally:
             website_entry.delete(0, END)
             website_entry.insert(0, "")
 
             password_entry.delete(0, END)
             password_entry.insert(0, "")
 
+# Search password
+def find_password():
+    website_is_found = False
+    website = website_entry.get()
+    try:
+        with open("data.json", mode="r") as file:
+            data = json.load(file)
+    except:
+        messagebox.showinfo(title="Error", message="You don't have any saved websites!")
+    else:
+        if website in data:
+            messagebox.showinfo(title=website, message=f"Your email on that website: {data[website]['email']}\n"
+                                                        f"Your password on that website:{data[website]['password']}")
+            password_entry.insert(0, data[website]["password"])
+            website_is_found = True
+        if not website_is_found:
+            messagebox.showinfo(title="Error", message="No matching websites found")
 
 # UI Setup
 window = Tk()
@@ -80,9 +110,9 @@ password_label.grid(row=3, column=0)
 
 
 # Entries
-website_entry = Entry(width=35)
+website_entry = Entry(width=21)
 website_entry.focus()
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
 
 email_entry = Entry(width=35)
 email_entry.insert(0, "boikoff.nikita@gmail.com")
@@ -98,6 +128,9 @@ generate_password_button.grid(row=3, column=2)
 
 add = Button(text="Add", width=36, command=save)
 add.grid(row=4, column=1, columnspan=2)
+
+search = Button(text="Search", command=find_password)
+search.grid(row=1, column=2)
 
 
 window.mainloop()
