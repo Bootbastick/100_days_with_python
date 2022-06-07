bfa433697beec747ff7abfd54f6886a0e44a50df
Bootbastick <boikoff.nikita@gmail.com>
2022-06-07 09:21:36 +0200

    Making a rain alert program, which emails the user to bring an umbrella if needed.

---
+++ Day 34/Day35/rain_alert/main.py
@@ -0,0 +1,35 @@
+import requests
+import smtplib
+
+api_key = "f40b9f9048d41be9d32329513caa28ec"
+MY_LAT = 42.453060
+MY_LONG = 18.537239
+MY_EMAIL = "bootbastick@yahoo.com"
+PASSWORD = "pfcmbhhjtiionwcy"
+
+
+params = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
+}
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=params)
+response.raise_for_status()
+weather_data = response.json()
+will_rain = False
+for i in range(12):
+    weather_id = weather_data["hourly"][i]["weather"][0]["id"]
+    if weather_id < 600:
+        will_rain = True
+
+if will_rain:
+    print("Bring umbrella")
+    connection = smtplib.SMTP_SSL("smtp.mail.yahoo.com", 465)
+    # connection.starttls()
+    connection.login(user=MY_EMAIL, password=PASSWORD)
+    connection.sendmail(from_addr=MY_EMAIL, to_addrs="boikoff.nikita@gmail.com",
+                        msg="Subject: Bad weather\n\nToday's going to be rainy."
+                            "Don't forget to bring an umbrella with you!")
+# 316
