d4d3111d18bf3e500b881b4196f9748c1406455b
Bootbastick <boikoff.nikita@gmail.com>
2022-07-13 21:29:08 +0200

    Moving around files of days from 41 to 44, making a top 100 movies you must watch list with python and beautiful sop and learning a little of website scrapping.

---
+++ Day45/main.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://news.ycombinator.com/news")
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+
+article_texts = []
+article_links = []
+articles = soup.find_all(name="a", class_="titlelink")
+for article_tag in articles:
+    article_text = article_tag.getText()
+    article_texts.append(article_text)
+    article_link = article_tag.get("href")
+    article_links.append(article_link)
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+print(article_texts)
+print(article_links)
+print(article_upvotes)
+
+biggest_score = max(article_upvotes)
+print(biggest_score)
+index = article_upvotes.index(biggest_score)
+print(article_texts[index])
+print(article_links[index])
+
+# 401
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import lxml
+#
+#
+# with open("website.html", encoding="utf8") as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, "html.parser")
+# print(soup.title.string)
